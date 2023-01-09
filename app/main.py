5aba0461fad99d18af6d338c59b1938957fba8b6
jnjc86 <78102451+jean834@users.noreply.github.com>
2023-01-09 17:28:06 -0500

    push to heroku

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "welcome to my api"}
+    return {"message": "welcome to my api succefully deployed from CI/CD pipeline"}
 
 
 
